32f4b76bc74309f8d2832ffbcaff55da98137d88
ahsam <ah.samba12@gmail.com>
2024-09-23 17:00:17 +0300

    css slider

--- index.php
+++ index.php
@@ -71,6 +71,67 @@
     #verticalCarousel .carousel-item-prev.carousel-item-right {
       transform: translateY(0);
     }
+
+
+    main{
+    width: min(1900px, 900vw);
+    margin: auto;
+}
+.slider{
+    width: 100%;
+    height: var(--height);
+    overflow: hidden;
+    mask-image: linear-gradient(
+        to right,
+        transparent,
+        #000 10% 90%,
+        transparent
+    );
+}
+.slider .list{
+    display: flex;
+    width: 100%;
+          height: 100%;
+
+    min-width: calc(var(--width) * var(--quantity));
+    position: relative;
+}
+.slider .list .item{
+    width: var(--width);
+    height: var(--height);
+    position: absolute;
+    left: 100%;
+    animation: autoRun 50s linear infinite;
+    transition: filter 0.5s;
+    animation-delay: calc( (50s / var(--quantity)) * (var(--position) - 1) - 50s)!important;
+}
+.slider .list .item img{
+    width: 100%;
+}
+@keyframes autoRun{
+    from{
+        left: 100%;
+    }to{
+        left: calc(var(--width) * -1);
+    }
+}
+.slider:hover .item{
+    animation-play-state: paused!important;
+    filter: grayscale(1);
+}
+.slider .item:hover{
+    filter: grayscale(0);
+}
+.slider[reverse="true"] .item{
+    animation: reversePlay 50s linear infinite;
+}
+@keyframes reversePlay{
+    from{
+        left: calc(var(--width) * -1);
+    }to{
+        left: 100%;
+    }
+}
   </style>
 <body>
 <div class="container">
@@ -124,6 +185,45 @@
 		</div>	
 	</div>
 </section>
+   <main>
+        <div class="slider" style="
+            --width: 200px;
+            --height: 150px;
+            --quantity: 1;
+        ">
+            <div class="list">
+                <div class="item" style="--position: 1"><img src="images/slider1_1.png" alt=""></div>
+                <div class="item" style="--position: 2"><img src="images/slider1_2.png" alt=""></div>
+                <div class="item" style="--position: 3"><img src="images/slider1_3.png" alt=""></div>
+                <div class="item" style="--position: 4"><img src="images/slider1_4.png" alt=""></div>
+                <div class="item" style="--position: 5"><img src="images/slider1_5.png" alt=""></div>
+                <div class="item" style="--position: 6"><img src="images/slider1_6.png" alt=""></div>
+                <div class="item" style="--position: 7"><img src="images/slider1_7.png" alt=""></div>
+                <div class="item" style="--position: 8"><img src="images/slider1_8.png" alt=""></div>
+                <div class="item" style="--position: 9"><img src="images/slider1_9.png" alt=""></div>
+                <div class="item" style="--position: 10"><img src="images/slider1_10.png" alt=""></div>
+            </div>
+        </div>
+
+        <div class="slider" reverse="true" style="
+            --width: 200px;
+            --height: 200px;
+            --quantity: 5;
+        ">
+            <div class="list">
+                <div class="item" style="--position: 1"><img src="images/services/services2.png" alt=""></div>
+                <div class="item" style="--position: 2"><img src="images/services/services2.png" alt=""></div>
+                <div class="item" style="--position: 3"><img src="images/services/services2.png" alt=""></div>
+                <div class="item" style="--position: 4"><img src="images/services/services2.png" alt=""></div>
+                <div class="item" style="--position: 5"><img src="images/services/services2.png" alt=""></div>
+                <div class="item" style="--position: 6"><img src="images/services/services2.png" alt=""></div>
+                <div class="item" style="--position: 7"><img src="images/services/services2.png" alt=""></div>
+                <div class="item" style="--position: 8"><img src="images/services/services2.png" alt=""></div>
+                <div class="item" style="--position: 9"><img src="images/services/services2.png" alt=""></div>
+            </div>
+        </div>
+    </main>
+
 
 
 <div id="carouselExample" class="carousel slide" data-ride="carousel">
